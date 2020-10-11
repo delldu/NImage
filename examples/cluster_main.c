b74465a40a0a67916ddaf214e8b718fd1d7e075b
Dell Du <18588220928@163.com>
2020-10-12 00:01:25 +0800

    Add image cluster command line for test.

---
+++ examples/cluster_main.c
@@ -0,0 +1,70 @@
+/************************************************************************************
+#***
+#***	File Author: Dell, Fri May 30 12:46:39 CST 2020
+#***
+#************************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <image.h>
+
+void cluster_help(char *cmd)
+{
+	printf("Usage: %s [option]\n", cmd);
+	printf("    -h, --help                   Display this help.\n");
+	printf("    -i, --input <image file>     Input image name\n");
+	printf("    -o, --output <image file>    Output image name\n");
+	printf("    -k, --class <number>         Cluster number, default is 32.\n");
+	exit(1);
+}
+
+int cluster_main(int argc, char **argv)
+{
+	int optc;
+	int option_index = 0;
+	char *input = NULL;
+	char *output = NULL;
+	int k = 32;
+
+	struct option long_opts[] = {
+		{ "help", 0, 0, 'h'},
+		{ "input", 1, 0, 'i'},
+		{ "output", 1, 0, 'o'},
+		{ "class", 1, 0, 'k'},
+		{ 0, 0, 0, 0}
+
+	};
+
+	if (argc <= 2)
+		cluster_help(argv[0]);
+	
+	while ((optc = getopt_long(argc, argv, "h i: o: k:", long_opts, &option_index)) != EOF) {
+		switch (optc) {
+		case 'i':	// Input
+			input = optarg;
+			break;
+		case 'o':	// Output
+			output = optarg;
+			break;
+		case 'k':	// Class
+			k = atoi(optarg);
+			break;
+		case 'h':	// help
+		default:
+			cluster_help(argv[0]);
+			break;
+	    	}
+	}
+
+	if (input && output) {
+		IMAGE *image = image_load(input);
+		color_cluster(image, k, 1);	// Update.
+		image_save(image, output);
+	}
+
+	// MS -- Modify Section ?
+
+	return 0;
+}
---
+++ examples/main.c
@@ -0,0 +1,98 @@
+/************************************************************************************
+***
+***	File Author: Dell, Fri May 30 12:46:39 CST 2020
+***
+************************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define CheckPoint(fmt, args...) printf("CheckPoint: %d(%s), " fmt "\n", __LINE__, __FILE__, ##args)
+
+
+typedef struct applet_s {
+	char *name;
+	int (*main) (int argc, char **argv);
+} applet_t;
+
+#include "applets.h"
+
+#define NUMBER_OF_APPLETS (sizeof(applet_table)/sizeof(applet_table[0]))
+
+static int applet_cmp(const void *a, const void *b)
+{
+	applet_t *m, *n;
+	m = (applet_t *) a;
+	n = (applet_t *) b;
+
+	return strcmp(m->name, n->name);
+}
+
+applet_t *applet_search(char *name)
+{
+	applet_t key;
+	key.name = name;
+
+	return (applet_t *)bsearch(&key, applet_table, NUMBER_OF_APPLETS, sizeof(applet_t), applet_cmp);
+}
+
+extern void exit(int status);
+
+void help(char *cmd)
+{
+	int i;
+	printf("This box is a multi-call binary that combines many applets.\n");
+	printf("Usage: %s applet-name\n", cmd);
+	printf("Support %ld applets:\n", NUMBER_OF_APPLETS);
+	for (i = 0; i < NUMBER_OF_APPLETS; i++)
+		printf("  %s", applet_table[i].name);
+	printf("\n");
+
+	exit(1);
+}
+
+int main(int argc, char **argv)
+{
+	int cnt;
+	char origname[256];
+	char *p, *appletname, *rootname;
+	applet_t *applet;
+
+	rootname = argv[0];
+
+	if ((cnt = readlink(argv[0], origname, sizeof(origname) - 1)) < 0 && strstr(argv[0], "imagebox")) {
+		/*
+		 * Guess: it must be box self !
+		 * We must use the following command to run:
+		 * busybox applet-name ......
+		 */
+		if (argc < 2)
+			help(argv[0]);
+
+		--argc;
+		argv++;
+	}
+
+	appletname = argv[0];
+	p = strrchr(appletname, '/');
+	if (p)
+		p++;
+	else
+		p = appletname;
+	appletname = p;
+
+	applet = applet_search(appletname);
+	if (applet) {
+		if (applet->main)
+			return applet->main(argc, argv);
+		else
+			printf("Fatal error: applet->main is NULL !\n");
+	} else {
+		help(rootname);
+	}
+
+	return 0;
+}
